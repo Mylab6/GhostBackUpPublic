cbd1efdc4bff03e4cfe47eb768608659a0a16b98
mylab6 <keithwilson@Keiths-MacBook-Air.local>
2023-06-20 21:07:41 -0400

    work

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,16 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 const path = require('path');
+const fs = require('fs');
 
 test('Download file', async ({ page }) => {
   // Set the download directory path
   const downloadDirectory = path.resolve(__dirname, 'downloads');
+
+  // Create the download directory if it doesn't exist
+  if (!fs.existsSync(downloadDirectory)) {
+    fs.mkdirSync(downloadDirectory, { recursive: true });
+  }
 
   await page.goto(process.env.GHOST_LAB_URL);
   await page.locator('[autocomplete="username"]').type(process.env.GHOST_LAB_USERNAME);
@@ -12,7 +18,7 @@ test('Download file', async ({ page }) => {
   await page.locator('#ember11').click();
 
   // Set the download directory for the current browser context
-  //await page.context().setDefaultDownloadOptions({ directory: downloadDirectory });
+  // await page.context().setDefaultDownloadOptions({ directory: downloadDirectory });
 
   await page.locator(`'[data-ember-action-328="328"]'`).click();
 
@@ -20,6 +26,8 @@ test('Download file', async ({ page }) => {
   const download = await page.waitForEvent('download');
 
   // Save the downloaded file
+  console.log(await download.path());
+
   const filePath = path.join(downloadDirectory, download.suggestedFilename);
   await download.saveAs(filePath);
 });
