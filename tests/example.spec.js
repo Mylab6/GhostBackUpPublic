21620133cd67d3990ccd5858c3fe1a38e94ade82
mylab6 <keithwilson@Keiths-MacBook-Air.local>
2023-06-21 17:03:57 -0400

    ok

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 const path = require('path');
 const fs = require('fs');
 
-test('Download file', async ({ page }) => {
-  test.slow();
+test('Download file', async ({ page, context }) => {
+
   // Set the download directory path
   page.on('console', msg => console.log(msg.text()))
 
@@ -15,7 +15,7 @@ test('Download file', async ({ page }) => {
     fs.mkdirSync(downloadDirectory, { recursive: true });
   }
 
-  await page.goto(process.env.GHOST_LAB_URL);
+  await page.goto(process.env.GHOST_LAB_URL + '/ghost/#/signin');
   await page.locator('[autocomplete="username"]').type(process.env.GHOST_LAB_USERNAME);
   await page.locator('[autocomplete="current-password"]').type(process.env.GHOST_LAB_PASSWORD);
  // await page.locator('#ember11').click();
@@ -24,6 +24,7 @@ test('Download file', async ({ page }) => {
   // Set the download directory for the current browser context
   // await page.context().setDefaultDownloadOptions({ directory: downloadDirectory });
   await page.locator('#ember66').click();
+
   await page.locator('span:text("Export")').click();
   // Wait for the download event to occur
   const download = await page.waitForEvent('download');
