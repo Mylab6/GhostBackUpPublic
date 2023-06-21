32be8ea1b5a78999cccb75874517fb3609d2e492
mylab6 <keithwilson@Keiths-MacBook-Air.local>
2023-06-20 20:14:23 -0400

    different_approach

--- backup-script.js
+++ backup-script.js
@@ -1,37 +1,16 @@
-const GhostAdminAPI = require('@tryghost/admin-api');
+const axios = require('axios');
 const fs = require('fs');
 
-const api = new GhostAdminAPI({
-  url: process.env.GHOST_URL,
-  key: process.env.GHOST_KEY,
-  version: 'v5.51.1'
-});
+const rssFeedUrl = process.env.GHOST_URL // Replace with the actual RSS feed URL
 
-// Prepare an object to hold all our data
-const backup = {
-  db: [{
-    meta: {
-      exported_on: Date.now(),
-      version: '4.48.9'
-    },
-    data: {}
-  }]
-};
-
-// Fetch all posts, tags, users, and settings
-Promise.all([
-  api.posts.browse({ limit: 'all' }).then(posts => (backup.db[0].data.posts = posts)),
-  api.tags.browse({ limit: 'all' }).then(tags => (backup.db[0].data.tags = tags)),
-  api.users.browse({ limit: 'all' }).then(users => (backup.db[0].data.users = users)),
-  api.settings.browseAll().then(settings => (backup.db[0].data.settings = settings))
-])
-  .then(() => {
-    // Write our data to a JSON file
-    fs.writeFile('ghost-backup.json', JSON.stringify(backup, null, 2), err => {
+axios.get(rssFeedUrl)
+  .then(response => {
+    const rssFeed = response.data;
+    fs.writeFile('rss-feed.xml', rssFeed, err => {
       if (err) {
         throw err;
       }
-      console.log('Backup complete!');
+      console.log('RSS feed saved!');
     });
   })
   .catch(err => {
